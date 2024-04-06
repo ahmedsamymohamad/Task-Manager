f7ed3660c19d9f834097c287f654b155853e4700
ahmedsamymohamad <ahmed01223330@gmail.com>
2024-04-06 23:06:52 +0200

    initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,32 @@
+'use client'
+
+import React from 'react'
+import {useSession, signIn, signOut} from "next-auth/react"
+import Link from 'next/link'
+
+const Navbar = () => {
+    const {data: session} = useSession()
+  return (
+    <header className='px-4 py-8'>
+        <div className='flex justify-between max-w-screen-xl mx-auto'>
+            <Link href={'/'}>TaskManager</Link>
+            <div className='flex'>
+                {session ? (
+                    <>
+                    <p className='mr-4'>{session.user.email}</p>
+                    <button onClick={() => signOut()}>signOut</button>
+                    </>
+                ) : (
+                    <>
+                    <button onClick={() => signIn()}>SignIn</button>
+                    </>
+                )
+                
+                }
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
